3c006f5a80cd59a989689fab2a7d17e80789e2bd
tekineta <111514255+tekineta@users.noreply.github.com>
2023-08-11 09:45:18 -0300

    Update Card.js

--- src/Componentes/Card.js
+++ src/Componentes/Card.js
@@ -2,14 +2,15 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { useNavigate } from "react-router-dom";
 
-function CardProducto({img = "", titulo="", texto =""}) {
+const CardProducto = (props/*{img = "", titulo="", texto =""}*/) => {
+  //console.log(props.product)
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img} />
+      <Card.Img variant="top" src={props.product.thumbnail} />
       <Card.Body>
-        <Card.Title>{titulo}</Card.Title>
+        <Card.Title>{props.product.title}</Card.Title>
         <Card.Text>
-          {texto}
+          {props.product.price}
         </Card.Text>
         <Button variant="primary">Ver Detalles</Button>
       </Card.Body>
